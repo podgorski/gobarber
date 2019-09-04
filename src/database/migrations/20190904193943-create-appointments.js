373a2863758885f0288cfe1f31413e97adf07718
diego2leite <diego@domob.me>
2019-09-04 16:52:50 -0300

    chore: model de agendamento

---
+++ src/database/migrations/20190904193943-create-appointments.js
@@ -0,0 +1,60 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        /*
+          Add altering commands here.
+          Return a promise to correctly handle asynchronicity.
+        */
+
+        return queryInterface.createTable('appointments', {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            date: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            user_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'users',
+                    key: 'id',
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'SET NULL',
+                allowNull: true,
+            },
+            provider_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'users',
+                    key: 'id',
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'SET NULL',
+                allowNull: true,
+            },
+            canceled_at: {
+                type: Sequelize.DATE,
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+
+    down: queryInterface => {
+        /*
+          Add reverting commands here.
+          Return a promise to correctly handle asynchronicity.
+        */
+        return queryInterface.dropTable('appointments');
+    },
+};
