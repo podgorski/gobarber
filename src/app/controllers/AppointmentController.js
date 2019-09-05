6ee499e9eb60b69e27ded099e6eba40c75b5b792
diego2leite <diego@domob.me>
2019-09-04 21:57:50 -0300

    feature: Listagem dos appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,37 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
+import File from '../models/File';
 import Appointment from '../models/Appointment';
 
 class AppointmentController {
+    async index(req, res) {
+        const appointments = await Appointment.findAll({
+            where: {
+                user_id: req.userId,
+                canceled_at: null,
+            },
+            order: ['date'],
+            attributes: ['id', 'date'],
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['id', 'name'],
+                    include: [
+                        {
+                            model: File,
+                            as: 'avatar',
+                            attributes: ['id', 'path', 'url'],
+                        },
+                    ],
+                },
+            ],
+        });
+
+        return res.json(appointments);
+    }
+
     async store(req, res) {
         const schema = Yup.object().shape({
             provider_id: Yup.number().required(),
@@ -28,6 +57,30 @@ class AppointmentController {
         if (!isProvider) {
             return res.status(401).json({
                 error: 'You can only create appointments with providers',
+            });
+        }
+
+        // Check for past dates
+        const hourStart = startOfHour(parseISO(date));
+
+        if (isBefore(hourStart, new Date())) {
+            return res.status(400).json({
+                error: 'Past dates are not permitted',
+            });
+        }
+
+        // Check date availability
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+
+        if (checkAvailability) {
+            return res.status(400).json({
+                error: 'Appointment date is not available',
             });
         }
 
--- src/routes.js
+++ src/routes.js
@@ -18,14 +18,20 @@ routes.get('/', async (req, res) => {
         message: 'Server running...',
     });
 });
+
 routes.post('/users', UserController.store);
+
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
