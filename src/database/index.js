df90e219dd7f9164cd804f2ae8acb851888b62d9
diego2leite <diego@domob.me>
2019-09-04 22:36:31 -0300

    chore: configuracao MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-
+import mongoose from 'mongoose';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
@@ -11,14 +11,27 @@ const models = [User, File, Appointment];
 class Database {
     constructor() {
         this.init();
+        this.mongo();
     }
 
     init() {
         this.connection = new Sequelize(databaseConfig);
 
-        models.map(model => model.init(this.connection));
-        models.map(
-            model => model.associate && model.associate(this.connection.models)
+        models
+            .map(model => model.init(this.connection))
+            .map(
+                model =>
+                    model.associate && model.associate(this.connection.models)
+            );
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            'mongodb://192.168.99.100:27017/gobarber',
+            {
+                useNewUrlParser: true,
+                useFindAndModify: true,
+            }
         );
     }
 }
