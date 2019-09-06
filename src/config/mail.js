e04a02453d87469504f02c225b2f73dfccaf29ba
diego2leite <diego@domob.me>
2019-09-06 00:20:10 -0300

    feature: Fila com Redis

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '8a60f2e9027166',
+        pass: '34bf35cb49d76e',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
