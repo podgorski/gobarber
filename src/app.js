ecc1c6f311e32696af92ce770f001a3940c07255
diego2leite <diego@domob.me>
2019-09-11 00:18:04 -0300

    feature: adicionado helmet para segurança

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import helmet from 'helmet';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
@@ -23,6 +24,7 @@ class App {
 
     middlewares() {
         this.server.use(Sentry.Handlers.requestHandler());
+        this.server.use(helmet());
         this.server.use(cors()); // DEV
         // this.server.use(cors({origin : "https://gobarber.com.br"})); // PRODUCTION
         this.server.use(express.json());
